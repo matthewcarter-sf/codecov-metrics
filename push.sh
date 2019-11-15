28a680050844dd727f072b8139d99305cdc4d54b
Matthew Carter <matthewcarter@scalefactor.com>
2019-11-15 11:42:53 -0600

    push.sh correct docker tag

--- push.sh
+++ push.sh
@@ -1,3 +1,2 @@
-# TODO: create this and replace with actual docker location
-# docker tag codecov-metrics:latest us.gcr.io/data-etl-227416/codecov-metrics:latest
-# docker push us.gcr.io/data-etl-227416/codecov-metrics:latest
+docker tag codecov-metrics:latest us.gcr.io/data-etl-227416/codecov-metrics:latest
+docker push us.gcr.io/data-etl-227416/codecov-metrics:latest
