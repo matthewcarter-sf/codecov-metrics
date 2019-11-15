c182559869cc424c226ff9b1616d40286a18ffed
Matthew Carter <matthewcarter@scalefactor.com>
2019-11-15 11:18:22 -0600

    running get_single_branch on client-services master branch

--- airflow-job.rb
+++ airflow-job.rb
@@ -1,3 +1,11 @@
 require 'dotenv/load'
+require_relative 'connectors/codecov'
 
-puts "pull"
+puts "Starting airflow-job execution for codecov-metrics"
+
+$codecov = Connectors::Codecov.new
+response = $codecov.get_single_branch('client-services', 'master')
+
+puts "response: #{response.dig('commit', 'totals', 'c')}%"
+
+puts "Finished airflow-job execution for codecov-metrics"
