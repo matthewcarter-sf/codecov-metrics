c3b4ce5999b48ff60e0a0f69599b2665bfc19522
Matthew Carter <matthewcarter@scalefactor.com>
2019-11-20 15:33:36 -0600

    add stuff

--- airflow-job.rb
+++ airflow-job.rb
@@ -3,32 +3,36 @@ require_relative 'connectors/codecov'
 
 puts "Starting airflow-job execution for codecov-metrics"
 
-repositories = [
-  'client-services',
-  'portal',
-  'backoffice'
+reports = [
+  { repo: 'client-services', branches: ['master'] },
+  { repo: 'portal', branches: ['master'] },
+  { repo: 'backoffice', branches: ['master'] }
 ]
 
 $codecov = Connectors::Codecov.new
 
-branch = 'master'
-data = repositories.map do |repo|
-  response = $codecov.get_single_branch(repo, branch)
-  coverage = response.dig('commit', 'totals', 'c')
-  {
-    repository: repo,
-    branch: branch,
-    coverage: coverage,
-    time: Time.now
-  }
-end.to_json
+time = Time.now
+data = {}
 
-puts "#{data}"
-
-if ENV.fetch('IS_DOCKER', false)
-  File.open('/airflow/xcom/return.json', 'w') do |file|
-    file.puts data
+reports.each do |report|
+  repo = report[:repo]
+  data[repo] = report[:branches].map do |branch|
+    puts "Requesting #{repo}-#{branch}"
+    response = $codecov.get_single_branch(repo, branch)
+    coverage = response.dig('commit', 'totals', 'c')
+    {
+      repository: repo,
+      branch: branch,
+      coverage: coverage,
+      time: time
+    }
   end
+end
+
+output_path = ENV.fetch('IS_DOCKER', false) ? '/airflow/xcom/return.json' : './return.json'
+
+File.open(output_path, 'w') do |file|
+  file.puts data.to_json
 end
 
 puts "Finished airflow-job execution for codecov-metrics"
